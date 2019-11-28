033e5489d02c8b49cf0e1ad18c8784294a6f02eb
Jeromeski <jeromegee@gmail.com>
2019-11-28 12:58:19 +0800

    created countdown timer component

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Carrousel from '../carrousel/carrousel.component';
+import Countdown from '../countdown/countdown.component';
 
 const Hero = () => {
   return (
     <div style={{
       position: 'relative'
-    }}>
+      }}>
       
       <Carrousel />
 
       <div className='artist_name'>
         <div className='wrapper'>Ariana Grande</div>
       </div>
+
+      <Countdown/>
+
     </div>
   );
 };
